268446eeea41f8c220dffe390e8f6564109e33c5
Abdurahmon086 <1937ali047@gmail.com>
2023-04-09 16:56:30 +0500

    change

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 const elWrapperBtns = document.querySelector(".hero-levels");
 const elEngWordsWrapper = document.querySelector(".main-words");
 const elUzWordsWrapper = document.querySelector(".uz-words");
-console.log(elWrapperBtns);
 
 elWrapperBtns.addEventListener("click", (evt) => {
   if (evt.target.classList.contains("hero-level__easy")) {
@@ -33,19 +32,56 @@ function randomWords(engWords, uzWords) {
 function renderWords(arr) {
   elEngWordsWrapper.innerHTML = "";
   elUzWordsWrapper.innerHTML = "";
+  let array = []
 
-  arr.forEach((item) => {
+  while (array.length < 10) {
+    const idWord = Math.floor(Math.random() * 10);
+    if (!array.includes(idWord)) array.push(idWord);
+  }
+
+
+  arr.forEach((item, i) => {
     const html = `
         <li class="game__item" >
-            <button class="btn game__btn" dataset-id=${item[0].id}>${item[0].word}</button>
+        <button class="btn game__btn" data-id=${item[0].id}>${item[0].word}</button>
         </li>`;
     const html2 = `
-        <li class="game__item"  >
-            <button class="btn game__btn" dataset-id=${item[1].id}>${item[1].word}</button>
+        <li class="game__item">
+            <button class="btn game__btn" data-id=${arr[array[i]][1].id}>${arr[array[i]][1].word}</button>
         </li>`;
     elEngWordsWrapper.insertAdjacentHTML("afterbegin", html);
-    elUzWordsWrapper.insertAdjacentHTML("afterbegin", html2);
+    elUzWordsWrapper.insertAdjacentHTML("afterbegin", html2);;
+
   });
 }
+
+// let elGame = document.querySelector('.game__wrapper')
+
+// let gameId1 = 0
+// let gameId2 = 0
+// let click1 = false, click2 = false;
+
+// elGame.addEventListener('click', (e) => {
+//   if (e.target.classList.contains('game__btn')) {
+//     const id = e.target.getAttribute('data-id');
+//     click1 = true
+//     gameId1 = id;
+//   }
+
+
+//   if (e.target.classList.contains('game__btn')) {
+//     const id = e.target.getAttribute('data-id');
+//     click2 = true
+//     gameId2 = id;
+//   }
+
+//   if (click1 === true && click2 === true) {
+//     if (gameId1 == gameId2) {
+//       console.log('sad');
+//     }
+//   }
+// })
+
+
 
 
